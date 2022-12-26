f5dedfce6a0b9deb9a22baf34b4e62618e612d6c
Mert musa Temel <mtemel@k2m14s04.42kocaeli.com.tr>
2022-12-26 17:17:51 +0300

    threads createt

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 16:20:29 by mtemel            #+#    #+#             */
-/*   Updated: 2022/11/30 17:14:30 by mtemel           ###   ########.fr       */
+/*   Updated: 2022/12/26 17:16:32 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,40 +19,58 @@ void	ft_error(char *str)
 	printf("%s\n", str);
 }
 
+void	*routine()
+{
+	printf("Test from thread!\n");
+	return (0);
+}
+
 /* create threads
 Note: pthread_create returns 0 on success
 if pthread_create does not return 0 write an error! */
-void	create_thread(t_philo *philo)
+void	create_thread(t_philo *philo, t_thread *thread)
 {
 	int	i;
 
+	(void)thread;
 	i = 0;
 	while (i < philo->philo_nof)
 	{
-		if (!pthread_create())
+		if (pthread_create(&philo->mythread[i].thread_id, NULL, &routine, NULL) != 0)
+		{
+			printf("Create error thread no:%d\n", i);
 			ft_error("Thread creation error!");
+		}
+		if (pthread_join(philo->mythread[i].thread_id, NULL) != 0)
+		{
+			printf("Join error thread no:%d\n", i);
+			ft_error("Thread join error!");
+		}
 		i++;
 	}
 }
 
 /* start of hunger games */
-void	start_to_thrive(t_philo *philo)
+void	start_to_thrive(t_philo *philo, t_thread *thread)
 {
-	create_thread(philo);
+	create_thread(philo, thread);
 }
 
 /* check arguments if there is nothing wrong then take as parameters */
-void	check_then_argtoparam(t_philo *philo, char **argv, int c)
+void	check_then_argtoparam(t_philo *philo, t_thread *thread, char **argv, int c)
 {
 	int	i;
 	int	j;
 
+	(void)thread;
 	i = 1;
 	while (argv[i])
 	{
 		j = 0;
 		while (argv[i][j])
 		{
+			if(j == 0 && argv[i][j] == '-')
+				j++;
 			if (!ft_isdigit(argv[i][j]))
 			{
 				ft_error("Arguments contain non-digit value!");
@@ -72,17 +90,23 @@ void	check_then_argtoparam(t_philo *philo, char **argv, int c)
 		philo->eat_noftep = ft_atoi(argv[5]);
 		printf("number_of_times_each_philosopher_must_eat: %d\n", philo->eat_noftep);
 	}
+	if (philo->philo_nof < 1)
+		ft_error("Number of philo can't be under 1!");
+	if (philo->sleep_tto < 0 || philo->eat_tto < 0 || philo->die_tto < 0 || philo->eat_noftep < 0)
+		ft_error("Times can not be negative!");
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo	*philo;
+	t_philo		*philo;
+	t_thread	*thread;
 
 	philo = (t_philo *)malloc(sizeof(t_philo));
+	thread = (t_thread *)malloc(sizeof(t_thread));
 	if (argc == 5 || argc == 6)
 	{
-		check_then_argtoparam(philo, argv, argc);
-		start_to_thrive(philo);
+		check_then_argtoparam(philo, thread, argv, argc);
+		start_to_thrive(philo, thread);
 	}
 	else
 		ft_error("Wrong Number Of Arguments!");
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 16:24:22 by mtemel            #+#    #+#             */
-/*   Updated: 2022/11/30 16:38:09 by mtemel           ###   ########.fr       */
+/*   Updated: 2022/12/26 17:02:08 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,26 @@
 # include <stdlib.h>
 # include <pthread.h>
 
+struct	s_philo;
+
+typedef struct s_thread
+{
+	int				philo_id;
+	int				left_fork_id;
+	int				right_fork_id;
+	pthread_t		thread_id;
+	pthread_t		fork_id;
+	struct s_philo	*myphilo;
+}	t_thread;
+
 typedef struct s_philo
 {
-	int	philo_nof;
-	int	die_tto;
-	int	eat_tto;
-	int	sleep_tto;
-	int	eat_noftep;
+	int			philo_nof;
+	int			die_tto;
+	int			eat_tto;
+	int			sleep_tto;
+	int			eat_noftep;
+	t_thread	mythread[500];
 }	t_philo;
 
 int	ft_atoi(const char *str);
